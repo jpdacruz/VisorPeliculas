c3828ed8dc7e06be5c23651dc2d0d96588384492
jpdacruz <jpdacruz@hotmail.com>
2020-03-26 14:06:37 -0300

    terminada carga de plantas

--- app/src/main/java/com/jpdacruz/visorpeliculas/data/Constantes.java
+++ app/src/main/java/com/jpdacruz/visorpeliculas/data/Constantes.java
@@ -2,8 +2,8 @@ package com.jpdacruz.visorpeliculas.data;
 
 public class Constantes {
 
+    //public static final String API_IMAGES_URL = "https://image.tmdb.org/t/p/w154/";
     public static final String API_IMAGES_URL = "https://image.tmdb.org/t/p/w500/";
-
     public static final String API_BASE_URL = "https://api.themoviedb.org/3/";
     public static final String API_KEY = "9c271d9e012943a41125c0e35ee126a2";
     public static final String DB_NAME = "movies";
